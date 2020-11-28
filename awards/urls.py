48e2626c5a101db973268f066cbb80ea8b5df3d7
sawe-daisy <deesawe16@gmail.com>
2020-11-28 14:27:31 +0300

    templates file and html files created

--- awards/urls.py
+++ awards/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('', PostListView.as_view(), name='gram-landing'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Image, Profile, Follow, Comment
+from django.http import HttpRequest, HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .forms import RegistrationForm, ProfileForm,ProfileUpdateForm, UserUpdateForm, ImageUploadForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from itertools import chain
+from django.urls import reverse
 
 # Create your views here.
+def register(request):
+    if request.method=="POST":
+        form=RegistrationForm(request.POST)
+        profForm=ProfileForm(request.POST, request.FILES)
+        if form.is_valid() and profForm.is_valid():
+            username=form.cleaned_data.get('username')
+            user=form.save()
+            profile=profForm.save(commit=False)
+            profile.user=user
+            profile.save()
+
+            messages.success(request, f'Successfully created Account!.You can now login as {username}!')
+        return redirect('login')
+    else:
+        form= RegistrationForm()
+        profForm=ProfileForm()
+    context={
+        'form':form,
+        'profForm': profForm
+    }
+    return render(request, 'users/register.html', context)
